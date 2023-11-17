e7e792aee58593ff218c16ec18da8bf25667e020
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-11-17 16:59:03 +0500

    Add refetch interval to the session provider.

--- src/components/providers/SessionProvider.jsx
+++ src/components/providers/SessionProvider.jsx
@@ -3,7 +3,7 @@ import { SessionProvider as Provider } from "next-auth/react";
 import React from "react";
 
 const SessionProvider = ({ children }) => {
-  return <Provider>{children}</Provider>;
+  return <Provider refetchInterval={5 * 60}>{children}</Provider>;
 };
 
 export default SessionProvider;
